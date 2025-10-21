292358ccefa31b384aa4bcfe8e0a6730a98a2eea
MdImranHossen01 <imranshuvo101@gmail.com>
2025-10-21 07:49:52 +0600

    fixed typescript error

---
+++ src/app/(mainlayout)/components/LeftComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftComponent = () => {
+    return (
+        <div>
+            <h1>this is left component</h1>
+        </div>
+    );
+};
+
+export default LeftComponent;
